110af13f1a11710c90cc51c845895d87babcc69d
Gtadros150485 <147589212+Gtadros150485@users.noreply.github.com>
2024-07-03 20:26:21 +0500

    Python revision

---
+++ hello_world.py
@@ -0,0 +1,59 @@
+box1 = 12 #Number
+box2 = "Steven" #String
+box3 = 3.1442342
+box4 = ["My First Name", "My Second Name"]
+list_of_numbers = [33,453,4353,5345,3]
+
+
+first_name = "George"
+second_name = "Michael"
+full_name = f" Hello  my name is {first_name} {second_name}"
+
+string1 = "python  "
+string2 = "python   "
+string3 = "python    "
+
+nostarch_url = 'https://httpsnos.comtarch.com'
+
+#Arth Operations
+
+long_number = 10_000_000_000
+
+PI_VALUE = 3.14
+print(PI_VALUE)
+PI_VALUE = 3.15
+print(PI_VALUE)
+
+list1 = [3, 3,3,3,10]
+print(list1)
+list1[0] = 5
+print(list1)
+list1.append(9)
+print(list1)
+list1.insert(0,7)
+print(list1)
+list1.pop()
+print(list1)
+list1.pop()
+print(list1)
+list1.pop(0)
+print(list1)
+list1.remove(3)
+print(list1)
+
+print(sorted(list1))
+print(len(list1))
+
+#define a tuple
+tupl = (3,9)
+print(tupl)
+
+#creating a function that add two variables
+def add_two(v1, v2):
+    return v1+v2
+def print_two_addition(v1,v2):
+    print(v1+v2)
+print(add_two(3,6))
+print_two_addition(3,7)
+
+
